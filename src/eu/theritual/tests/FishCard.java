b375c411a010f5108be337ae42d9225c914e5f02
TheRitual <dev@theritual.eu>
2019-07-07 18:09:44 +0200

    Added card ID

--- src/eu/theritual/tests/FishCard.java
+++ src/eu/theritual/tests/FishCard.java
@@ -1,24 +1,25 @@
 package eu.theritual.tests;
 
-import javax.smartcardio.Card;
-
 public class FishCard {
     private final CognitusTypes.Cards type;
+    private final String id;
     private final FishCardSide sideA;
     private final FishCardSide sideB;
     private final FishCardSide sideC;
 
-    FishCard(FishCardSide sideA, FishCardSide sideB, FishCardSide sideC) {
+    FishCard(FishCardSide sideA, FishCardSide sideB, FishCardSide sideC, String id) {
         this.type = CognitusTypes.Cards.THREE_SIDES;
         this.sideA = sideA;
         this.sideB = sideB;
         this.sideC = sideC;
+        this.id = id;
     }
 
-    FishCard(FishCardSide sideA, FishCardSide sideB) {
+    FishCard(FishCardSide sideA, FishCardSide sideB, String id) {
         this.type = CognitusTypes.Cards.TWO_SIDES;
         this.sideA = sideA;
         this.sideB = sideB;
         this.sideC = null;
+        this.id = id;
     }
 }
